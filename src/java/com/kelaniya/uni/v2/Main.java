1342e03a4dd19d69cf0a4d237fa53523a9a490e0
Hasini Samarathunga <hasinisamarathunga@Hasinis-MacBook-Air.local>
2021-08-22 21:00:16 +0530

    v2 - final code

--- src/java/com/kelaniya/uni/v2/Main.java
+++ src/java/com/kelaniya/uni/v2/Main.java
@@ -1,25 +1,52 @@
 package com.kelaniya.uni.v2;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.io.IOException;
+import java.util.List;
+
 public class Main {
 
-    public static void main(String[] args) {
+
+    //SOLID PRINCIPLES --> ALL THE SOLID PRINCIPLES ARE VIOLATED HERE!!!
+    public static void main(String[] args) throws IOException {
 
         //Make sure to validate the arguments before using...
-        if (args.length == 0){
+        if (args.length == 0) {
             System.out.println("Please provide the operation as an argument");
             return;
         }
 
-        //Another validation that we could do to these args?
-        //add, sub, mul, div
         String operator = args[0];
 
-        if(!(operator.equals("add") || operator.equals("sub") || operator.equals("mul"))){
+        if (!(operator.equals("add") || operator.equals("sub") || operator.equals("mul"))) {
             System.out.println("Please provide add,sub or mul as the operator argument");
             return;
         }
 
-        System.out.println(args[0]);
+        //read the numbers text file
+        List<String> numbersStrs = Files.readAllLines(
+                Paths.get("/Users/hasinisamarathunga/Desktop/calculator/numbers.txt")
+        );
+
+        double number1 = Double.parseDouble(numbersStrs.get(0));
+        double number2 = Double.parseDouble(numbersStrs.get(1));
+
+        double result = 0;
+
+        switch (operator) {
+            case "add":
+                result = number1 + number2;
+                break;
+            case "sub":
+                result = number1 - number2;
+                break;
+            case "mul":
+                result = number1 * number2;
+                break;
+        }
+
+        System.out.println("The result is " + result);
 
     }
 }
