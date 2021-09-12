f60242700b68b61347b35a6e51f5739daafa2854
Hasini Samarathunga <hasinisamarathunga@Hasinis-MacBook-Air.local>
2021-09-12 17:32:14 +0530

    v5 - integrated Dependency Inversion Principle

---
+++ src/java/com/kelaniya/uni/v5/operation/OperationFactory.java
@@ -0,0 +1,28 @@
+package com.kelaniya.uni.v5.operation;
+
+//Factory design pattern
+//When you make a object after making a decision
+public class OperationFactory {
+
+    public Operation getInstance(String operator) {
+
+        Operation operation = null;
+        switch (operator) {
+            case "add":
+                operation = new AddOperation();
+                break;
+            case "sub":
+                operation = new SubOperation();
+                break;
+            case "mul":
+                operation = new MulOperation();
+                break;
+            case "div":
+                operation = new DivOperation();
+                break;
+        }// violation of OCP -> this is a known violation since factory design pattern
+        // -> in order to fix this we need to apply a dependency injection framework like Spring framework
+
+        return operation;
+    }
+}
